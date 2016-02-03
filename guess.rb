03686efccb0b80e8ce23b64d9181a5e541b6f5b2
lawrencejames <lawrence.james@live.com>
2016-02-02 21:46:21 -0500

    Added random number generator 0-10

--- guess.rb
+++ guess.rb
@@ -4,3 +4,7 @@
 puts "Here are the rules to play the game!"
 
 puts "Guess and enter a number between 1 and 10.\nYou only get three (3) gueses.\nGuess correctly and you get undying praise.\nGuess wrong and will look at you with derision and contempt!\n"
+#generates random number between 1-10.
+#need to figure out how to not have zero
+random_number = rand(10)
+puts random_number #output only for testing
