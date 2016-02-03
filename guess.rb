07445fe37e72c98f83a9306cae6b1105fb2616a5
lawrencejames <lawrence.james@live.com>
2016-02-02 21:34:38 -0500

    Corrected instructions

--- guess.rb
+++ guess.rb
@@ -3,4 +3,4 @@
 #Rules - Tell the player the rules
 puts "Here are the rules to play the game!"
 
-puts "Guess a number between 1 and 10.\nYou only get three (3) gueses.\nGuess correctly and you get undying praise.\nGuess wrong and will look at you with derision and contempt!\n"
+puts "Guess and enter a number between 1 and 10.\nYou only get three (3) gueses.\nGuess correctly and you get undying praise.\nGuess wrong and will look at you with derision and contempt!\n"
